fd64f81459321a43bcc46ebb8e526326fe06f9e9
Vladimir Dimitrieski <dimitrieski89@gmail.com>
2023-02-14 20:43:23 +0100

    moj prvi commit

---
+++ src/main/java/com/iktpreobuka/t9/ExampleGitG2Application.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.t9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGitG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGitG2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iktpreobuka/t9/ExampleGitG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.t9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGitG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
